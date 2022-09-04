346186cfff46141552ccaac073685b4ee7c0f33f
Fschatbot <s.himanshu.sultania@fountainheadschools.org>
2022-09-04 14:10:36 +0530

    Made the color of the settings icon permanently white

--- script.js
+++ script.js
@@ -29,7 +29,6 @@ function ChangeColor(hex, update_input = false) {
 		.join(", ");
 	if (update_input) document.querySelector("#color-picker").value = color.hex();
 	if (window.history.replaceState) window.history.replaceState(null, "Color Share", "?color=" + color.hex());
-	document.getElementById("settings").style.setProperty("--inv-color", color.negate().hex());
 	// Fetch Nearest Color Name
 	fetch(`https://api.color.pizza/v1/${color.hex().substring(1)}`)
 		.then((res) => res.json())
